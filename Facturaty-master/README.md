b0ef2ce294ea9eb9b9f92601ae13d7aba805dc04
youcefleg <51298387+youcefleg@users.noreply.github.com>
2020-04-06 02:38:15 +0100

    Add files via upload

---
+++ Facturaty-master/README.md
@@ -0,0 +1,16 @@
+# Facturaty
+Simple application de Gestion des Factures de vente
+# tools
+* JavaFx Scene Builder 
+* IDE Intellij 
+* MySql 
+
+# Preview
+
+![](screenshots/login.JPG)        ![](screenshots/add.JPG)
+
+![](screenshots/addFa.JPG)
+
+![](screenshots/facture.JPG)
+
+![](screenshots/FactureClientByDate.JPG)
---
+++ Facturaty-master/src/connexion/Myconnexion.java
@@ -0,0 +1,27 @@
+package connexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Connection;
+/**
+ * Created by Youcef LEG on 10/02/2019.
+ */
+public class Myconnexion {
+    Connection con;
+
+    public Connection getConnection() {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/base", "root", "yourpassword");
+        //here base is the database name;
+        //root is the default username;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return con;
+    }
+}
